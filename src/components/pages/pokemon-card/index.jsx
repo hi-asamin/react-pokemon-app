e8cdafec55a0b4e0ff672727fb4260f283d8986c
marc-town <asamin.itconsultant@gmail.com>
2020-12-19 23:49:44 +0900

    #8 fix card centering

--- src/components/pages/pokemon-card/index.jsx
+++ src/components/pages/pokemon-card/index.jsx
@@ -46,12 +46,11 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from 'react-router-dom';
 
-import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
 import { getPokemon } from '../../../usecases/pokemon';
@@ -67,26 +66,28 @@ export const PokemonCard = (props) => {
     });
   });
 
+  const cardStyle = {
+    margin: 'auto',
+    textAlign: 'center',
+  }
+
   return (
     <>
       {pokemon && (
-        <Card>
-          <CardActionArea>
-            <CardMedia>
-              <img src={pokemon.sprites.front_default} alt='' />
-            </CardMedia>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-              {pokemon.name}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary" onClick={() => { history.push(`/pokemon/${pokemon.name}`) }}>
-              Show More
-            </Button>
-          </CardActions>
-        </Card>
+        <Link onClick={() => { history.push(`/pokemon/${pokemon.name}`) }}>
+          <Card style={cardStyle}>
+            <CardActionArea>
+              <CardMedia>
+                <img src={pokemon.sprites.front_default} alt='' />
+              </CardMedia>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                {pokemon.name}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Link>
       )}
     </>
   );
